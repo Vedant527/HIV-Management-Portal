7b1c57ef3180bb35e8b2100776bd35201d8d2477
HennaMian <43707978+HennaMian@users.noreply.github.com>
2023-04-23 20:19:13 -0400

    Add files via upload

---
+++ views/app.js
@@ -0,0 +1,289 @@
+// Import required libraries
+const express = require('express');
+const admin = require('firebase-admin');
+const session = require('express-session');
+const crypto = require('crypto');
+
+// Initialize the Express app
+const app = express();
+
+// Initialize Firebase Admin SDK with your service account credentials
+const serviceAccount = require('./pass.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://hiv-management-db-default-rtdb.firebaseio.com/'
+});
+
+// Set the view engine to EJS and configure the views directory
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Start the server
+const port = process.env.PORT || 3003;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// Setup Express Session
+app.use(session({
+  secret: crypto.randomBytes(32).toString('hex'),
+  resave: false,
+  saveUninitialized: false
+}));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// LOGIN PAGE
+app.get('/', (req, res) => {
+  if (req.session.userId != null) {
+    res.render('home');
+  } else {
+    res.render('login', { message: null});
+  }
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.render('login', { message: 'Please provide an email and password' });
+    }
+  
+    admin.auth().getUserByEmail(email)
+      .then((userRecord) => {
+        const uid = userRecord.uid;
+        req.session.userId = uid;
+        return admin.auth().createCustomToken(uid)
+          .then((customToken) => {
+            return res.render('home', { token: customToken });
+          })
+          .catch((error) => {
+            console.log(error);
+            return res.render('login', { message: 'Error creating custom token' });
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        const errorMessage = 'Incorrect username or password. Please try again.';
+        return res.render('login', { message: errorMessage });
+      });
+  });
+
+
+  // SIGNUP PAGE
+  app.get('/signup', (req, res) => {
+    res.render('signup', {message: null});
+  });
+
+  app.post('/signup', async (req, res) => {
+    const { email, username, password } = req.body;
+    try { 
+      // see if account exists
+      await admin.auth().getUserByEmail(email).then(() => {
+        // Email is already registered
+        res.render('signup', { message: 'Email is already registered' });
+      })
+      // Create a new user account in Firebase Authentication
+      const userRecord = await admin.auth().createUser({
+        email,
+        password
+      });
+      
+      // Store the user's email, username, and password in the Firebase Realtime Database
+      await admin.database().ref('users').child(userRecord.uid).set({
+        email,
+        username,
+        password
+      });
+  
+      res.send('User account created successfully!');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error creating user account');
+    }
+  });
+
+  // HOME PAGE
+
+  app.get('/home', (req, res) => {
+    if (req.session.userId != null) {
+      res.render('home');
+    } else {
+      res.render('login', { message: null});
+    }
+  });
+
+
+
+
+  // EXERCISE PAGE
+  app.get('/exercise', function(req, res) {
+    if (req.session.userId == null) {
+      res.render('login', { message: null});
+    } else {
+      const currUser = req.session.userId;
+      const events = [];
+  
+      admin.database().ref('users').child(currUser).orderByChild('date').on('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          const childData = childSnapshot.val();
+          const event = {
+            title: childData.exercise,
+            start: childData.date
+          };
+          if (childData.exercise) {
+            events.push(event);
+            // event['calories'] = childData.calories;
+          }
+          if (childData.length) {
+            event['length'] = childData.length;
+          }
+          // events.push(event);
+        });
+        res.render('exercise', {events: events});
+      });
+    }
+  });
+  
+  app.post('/exercise', function(req, res) {
+    const exercise = req.body.exercise;
+    const date = req.body.date;
+    const length = req.body.length;
+    // const calories = req.body.calories;
+  
+    //store data in database
+    const currUser = req.session.userId;
+    admin.database().ref('users').child(currUser).push({
+      exercise: exercise,
+      date: date,
+      length: length
+      // calories: calories
+    });
+    res.redirect('/exercise');
+  });
+
+
+
+// APPOINTMENTS PAGE
+  app.get('/appointments', function(req, res) {
+    if (req.session.userId == null) {
+      res.render('login', { message: null});
+    } else {
+      const currUser = req.session.userId;
+      const userRef = admin.database().ref('users').child(currUser);
+  
+      userRef.orderByChild('type').on('value', function(snapshot) {
+        const appointments = [];
+        snapshot.forEach(function(childSnapshot) {
+          const childData = childSnapshot.val();
+          if (childData.hasOwnProperty('type') && childData.hasOwnProperty('date') && childData.hasOwnProperty('time')) {
+            appointments.push(childData);
+          }
+        });
+        var message = '';
+        res.render('appointments', { appointments: appointments, message: message });
+      });
+    }
+  });
+  
+  app.post('/appointments', function(req, res) {
+    const type = req.body.type;
+    const date = req.body.date;
+    const time = req.body.time;
+    const currUser = req.session.userId;
+    const appointmentsRef = admin.database().ref('users').child(currUser);
+    let exists = false;
+  
+    appointmentsRef.orderByChild('date').equalTo(date).once('value', function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        const childData = childSnapshot.val();
+        if (childData.hasOwnProperty('type') && childData.hasOwnProperty('date') && childData.hasOwnProperty('time')) {
+          if (childData.type === type && childData.time === time && childData.date === date) {
+            exists = true;
+          }
+        }
+      });
+      if (exists) {
+        const appointments = [];
+        appointmentsRef.orderByChild('date').on('value', function(snapshot) {
+          snapshot.forEach(function(childSnapshot) {
+            const childData = childSnapshot.val();
+            if (childData.hasOwnProperty('type') && childData.hasOwnProperty('date') && childData.hasOwnProperty('time')) {
+              appointments.push(childData);
+            }
+          });
+          res.render('appointments', { appointments: appointments, message: "Appointment already exists" });
+        });
+      } else {
+        appointmentsRef.push({
+          type: type,
+          date: date,
+          time: time,
+        }, function(error) {
+          if (error) {
+            console.log("Error adding appointment:", error);
+          } else {
+            const appointments = [];
+            appointmentsRef.orderByChild('date').on('value', function(snapshot) {
+              snapshot.forEach(function(childSnapshot) {
+                const childData = childSnapshot.val();
+                if (childData.hasOwnProperty('type') && childData.hasOwnProperty('date') && childData.hasOwnProperty('time')) {
+                  appointments.push(childData);
+                }
+              });
+              res.render('appointments', { appointments: appointments, message: "Appointment successfully created" });
+            });
+          }
+        });
+      }
+    });
+  });
+  
+
+   // DIET PAGE
+   app.get('/diet', function(req, res) {
+    if (req.session.userId == null) {
+      res.render('login', { message: null});
+    } else {
+      const currUser = req.session.userId;
+      const events = [];
+  
+      admin.database().ref('users').child(currUser).orderByChild('date').on('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          const childData = childSnapshot.val();
+          const event = {
+            title: childData.type,
+            start: childData.date,
+          };
+          if (childData.servings) {
+            events.push(event);
+          }
+          if (childData.servings) {
+            event['servings'] = childData.servings;
+            event['dish'] = childData.dish;
+          }
+        });
+        res.render('diet', {events: events});
+      });
+    }
+  });
+  
+  app.post('/diet', function(req, res) {
+    const type = req.body.type;
+    const date = req.body.date;
+    const dish = req.body.dish;
+    const servings = req.body.servings;
+    // const calories = req.body.calories;
+  
+    //store data in database
+    const currUser = req.session.userId;
+    admin.database().ref('users').child(currUser).push({
+      type: type,
+      date: date,
+      dish: dish,
+      servings: servings
+      // calories: calories
+    });
+    res.redirect('/diet');
+  });
